96b8321da0da70328605c9000cce415518bef857
basharkhan7776 <basharkhan7776@gmail.com>
2025-04-14 19:16:13 +0530

    init vercel deployment and clerk

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,32 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPublishableKey) {
+  throw new Error("Missing Publishable Key")
+}
+
+
 
 // Ensure the DOM is fully loaded before trying to access elements
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('root');
-  
+
   if (!container) {
     console.error('Failed to find the root element');
     return;
   }
-  
+
   try {
     const root = createRoot(container);
     root.render(
       <React.StrictMode>
-        <App />
+        <ClerkProvider publishableKey={clerkPublishableKey}>
+          <App />
+        </ClerkProvider>
       </React.StrictMode>
     );
   } catch (error) {
